c8bfee52e00968af8c42ec302115e4d0984ab143
keithb <47063067+aaaa-trsh@users.noreply.github.com>
2020-12-30 21:31:52 -0600

    Initial commit

---
+++ Advanced Control/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,40 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.util.Units;
+
+public final class Constants {
+    public static class DriveConstants
+    {
+        public static int[] kLeftMotorPorts = {0, 1};
+        public static int[] kRightMotorPorts = {2, 3};
+
+        public static int[] kLeftEncoderPorts = {0, 1};
+        public static int[] kRightEncoderPorts = {2, 3};
+
+        public static double kS = 0.99;
+        public static double kV = 7.31;
+        public static double kA = 0.443;
+
+        public static double kP = 1.1;
+        public static double kI = 0;
+        public static double kD = 0;
+
+        public static double kTurnP = 0;
+        public static double kTurnI = 0;
+        public static double kTurnD = 0;
+
+        public static int kDriveEncoderResolution = 360;
+        public static double kWheelDiameter = 6; // inches
+        public static double kDriveEncoderDPP = (kWheelDiameter * Math.PI)/kDriveEncoderResolution/12   ;
+
+        public static double kMaxVelocity = 3; // feet / s
+        public static double kMaxAcceleration = 3; // feet / s^2
+
+        public static double kTrackwidth = 0.6397603419838763; // feet
+    }
+
+    public static class OIConstants
+    {
+        public static int kDriveControllerPort = 0;
+    }
+}
---
+++ Advanced Control/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,84 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import java.util.Arrays;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.controller.RamseteController;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.util.Units;
+import frc.robot.subsystems.*;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+public class RobotContainer {
+    private final DriveSubsystem drivebase = new DriveSubsystem();
+    Compressor compressor = new Compressor();
+    Joystick joystick = new Joystick(0);
+
+    public RobotContainer() {
+        //compressor.start();
+        configureButtonBindings();
+    }
+
+    private void configureButtonBindings() {
+        drivebase.setDefaultCommand(new RunCommand(() -> drivebase.arcadeDrive(joystick.getY(), joystick.getX()), drivebase));
+        JoystickButton button = new JoystickButton(joystick, 1);
+        button.whenPressed(new InstantCommand(()->drivebase.setTransmission(true)))
+            .whenReleased(new InstantCommand(()->drivebase.setTransmission(false)));
+    }
+
+    public void log()
+    {
+        SmartDashboard.putNumber("Rotation", drivebase.getGyroHeading().getDegrees());
+        SmartDashboard.putNumber("X", drivebase.getPose().getTranslation().getX());
+        SmartDashboard.putNumber("Y", drivebase.getPose().getTranslation().getY());
+        SmartDashboard.putNumber("left", drivebase.getLeftEncoder().getDistance());
+        SmartDashboard.putNumber("right", drivebase.getRightEncoder().getDistance());
+    }
+    public Command getAutonomousCommand() {
+        TrajectoryConfig config = new TrajectoryConfig(
+            Units.feetToMeters(2.0), Units.feetToMeters(2.0));
+        config.setKinematics(drivebase.getKinematics());
+    
+        Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
+            Arrays.asList(new Pose2d(), new Pose2d(3.0, 2, Rotation2d.fromDegrees(20))),
+            config
+        );
+    
+        RamseteCommand command = new RamseteCommand(
+            trajectory,
+            drivebase::getPose,
+            new RamseteController(2, .7),
+            drivebase.getFeedforward(),
+            drivebase.getKinematics(),
+            drivebase::getSpeeds,
+            drivebase.getLeftPIDController(),
+            drivebase.getRightPIDController(),
+            drivebase::tankDriveVolts,
+            drivebase
+        );
+    
+        return command.andThen(() -> drivebase.tankDriveVolts(0, 0));
+    }
+
+    public void reset()
+    {
+        drivebase.resetOdometry();
+    }
+}
